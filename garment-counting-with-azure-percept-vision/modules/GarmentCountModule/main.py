2e7aa9d992bf55eb9fed883f829955f3442d472c
Rajesh Dhiman <rajeshdhiman1973@gmail.com>
2022-03-14 12:11:51 -0600

    Add files via upload

---
+++ garment-counting-with-azure-percept-vision/modules/GarmentCountModule/main.py
@@ -0,0 +1,155 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import os
+import asyncio
+from six.moves import input
+import threading
+from azure.iot.device.aio import IoTHubModuleClient
+from azure.iot.device import MethodResponse, Message
+import json
+from datetime import datetime
+from datetime import datetime, timezone
+
+#For Speech
+import time
+import sys
+
+async def main():
+    # The client object is used to interact with your Azure IoT hub.
+    module_client = IoTHubModuleClient.create_from_edge_environment()
+
+    # connect the client.
+    await module_client.connect()
+
+    # event indicating when user is finished
+    finished = threading.Event()
+
+    # Define behavior for receiving an input message on input1 and input2
+    # NOTE: this could be a coroutine or a function
+    async def message_handler(input_message):
+        if input_message.input_name == "CountInput":
+            now = datetime.now()
+            print(f'{now} The data in the message received on azureeyemodule was {input_message.data}')
+            print(f'{now} Custom properties are {input_message.custom_properties})')
+
+            inference_list = json.loads(input_message.data)['NEURAL_NETWORK']
+            print(f'inference list: {inference_list}')
+            count_garment = 0 
+            
+            if isinstance(inference_list, list) and inference_list:
+                now = datetime.fromtimestamp(int(inference_list[0]['timestamp'][:-9]))
+                
+                count_garment = len(inference_list)
+            
+            print(f'Garment_Count: {count_garment}')
+            print(f'Date: {now}')
+
+            json_data = {
+                    'Date': f'{now}', 
+                    'Garment_Count': count_garment
+                }
+            
+            print("forwarding mesage to output1")
+            msg = Message(json.dumps(json_data))
+            msg.content_encoding = "utf-8"
+            msg.content_type = "application/json"
+            await module_client.send_message_to_output(msg, "output1")
+
+        else:
+            print("message received on unknown input")
+
+    # Define behavior for receiving a twin desired properties patch
+    # NOTE: this could be a coroutine or function
+    def twin_patch_handler(patch):
+        print("the data in the desired properties patch was: {}".format(patch))
+
+    # Define behavior for receiving methods
+    async def method_handler(method_request):
+        print("Unknown method request received: {}".format(method_request.name))
+        method_response = MethodResponse.create_from_method_request(method_request, 400, None)
+        await module_client.send_method_response(method_response)
+
+    # set the received data handlers on the client
+    module_client.on_message_received = message_handler
+    module_client.on_twin_desired_properties_patch_received = twin_patch_handler
+    module_client.on_method_request_received = method_handler
+
+    #add speech code here     
+    SpeechMain(module_client,finished)
+
+    # This will trigger when a Direct Method Request for "shutdown" is sent.
+    # NOTE: This sample will NOT exit until a Direct Method Request is sent.
+    # Send one using the Azure IoT Explorer or the Azure IoT CLI
+    # (https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-devguide-direct-methods)
+    finished.wait()
+    # Once it is received, shut down the client
+    await module_client.shutdown()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+    # If using Python 3.6 or below, use the following code instead of asyncio.run(main()):
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.close()
+
+
+#ForSpeech Module
+
+async def SpeechMain(module_client,finished):
+    
+    try:
+
+        # define behavior for receiving an input message on input1
+        async def input1_listener(module_client):
+            while True:
+                input_message = await module_client.receive_message_on_input("EarInput")  # blocking call
+                text_data = input_message.data.decode('utf-8')
+                dict_data = json.loads(text_data)
+                
+
+                print("the data in the message received on input1 was ")
+                print(input_message.data)
+                print("the data text in the message received on input1 was ")
+                print(text_data)
+                print("the object in the message received on input1 was ")
+                print(dict_data)
+
+                if 'botResponse' in dict_data:
+                    resp = dict_data["botResponse"]
+                    print(f'resp: {resp}')
+
+                    if resp == 'Ok Starting now. Please Start scanning your selected garments':
+                        print('Received Start.')
+                        
+                    if resp == 'Stopped Scanning.':
+                        print('Received Stop.')
+                
+                    if resp == 'Please scan your next set of garments.':
+                        print('Received Next.')
+                
+
+                print("custom properties are")
+                print(input_message.custom_properties)
+                print("forwarding mesage to output1")
+                await module_client.send_message_to_output(input_message, "output1")
+
+        # Schedule task for C2D Listener
+        listeners = asyncio.gather(input1_listener(module_client))
+
+        print ( "waiting for Command/messages. ")
+     
+        finished.wait()
+
+        # Cancel listening
+        listeners.cancel()
+
+    except Exception as e:
+        print ( "Unexpected error %s " % e )
+        raise
+
+
